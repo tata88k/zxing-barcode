2fd50e71b1ed0b6b3b55d6c90b608f0716ec8057
UsherBaby <UsherBaby@users.noreply.github.com>
2016-03-16 10:39:55 +0800

    Prepare for camera2 api
    Prepare for camera2 api

--- Barcode/app/src/main/java/com/pacific/barcode/CameraManager2.java
+++ Barcode/app/src/main/java/com/pacific/barcode/CameraManager2.java
@@ -3,7 +3,7 @@ package com.pacific.barcode;
 import android.annotation.TargetApi;
 
 /**
- * This class is for android 5.0 or above and it uses camera2 api
+ * For android 5.0 or above and it uses camera2 api
  */
 @TargetApi(21)
 public class CameraManager2 {
